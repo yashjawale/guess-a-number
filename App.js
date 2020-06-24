704c6e4cf7bc89b289e9eb8c558da7187628e3dc
Yash-Jawale <63059729+Yash-Jawale@users.noreply.github.com>
2020-06-24 21:03:51 +0530

    Added Header and initialized Start Game Screen

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import Header from "./components/Header.js";
+import StartGameScreen from "./screens/StartGameScreen.js";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	return (
+		<View style={styles.screen}>
+			<Header title="Guess a Number" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	screen: {
+		flex: 1,
+	},
 });
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const StartGameScreen = (props) => {
+	return <View></View>;
+};
+
+export default StartGameScreen;
