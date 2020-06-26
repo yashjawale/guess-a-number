b20c063dd86cefd32ce19171d2955c653f495158
Yash-Jawale <63059729+Yash-Jawale@users.noreply.github.com>
2020-06-26 16:44:33 +0530

    Added more Input Validation and Conversion and created state for Chosen Number

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -14,10 +14,33 @@ import Colors from "../constants/colors.js";
 
 const StartGameScreen = (props) => {
 	const [enteredValue, setEnteredValue] = useState("");
+	const [confirmed, setConfirmed] = useState(false);
+	const [selectedNumber, setSelectedNumber] = useState();
 
 	const numberInputHandler = (inputText) => {
 		setEnteredValue(inputText.replace(/[^0-9]/g, ""));
 	};
+
+	const resetInputHandler = () => {
+		setEnteredValue("");
+		setConfirmed(false);
+	};
+
+	const confirmInputHandler = () => {
+		const chosenNumber = parseInt(enteredValue);
+		if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+			return;
+		}
+		setConfirmed(true);
+		setSelectedNumber(parseInt(enteredValue));
+		setEnteredValue("");
+	};
+
+	let confirmedOutput;
+
+	if (confirmed) {
+		confirmedOutput = <Text>Your Chosen Number: {selectedNumber}</Text>;
+	}
 
 	return (
 		<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -37,17 +60,22 @@ const StartGameScreen = (props) => {
 					/>
 					<View style={styles.buttonContainer}>
 						<View style={styles.button}>
-							<Button title="Reset" color={Colors.accent} onPress={() => {}} />
+							<Button
+								title="Reset"
+								color={Colors.accent}
+								onPress={resetInputHandler}
+							/>
 						</View>
 						<View style={styles.button}>
 							<Button
 								title="Confirm"
 								color={Colors.primary}
-								onPress={() => {}}
+								onPress={confirmInputHandler}
 							/>
 						</View>
 					</View>
 				</Card>
+				{confirmedOutput}
 			</View>
 		</TouchableWithoutFeedback>
 	);
