2241de7629dfb7efbab123f26f56ee3564fa47e0
Yash-Jawale <63059729+Yash-Jawale@users.noreply.github.com>
2020-06-26 14:17:26 +0530

    Disintegrated Card Element and Created constants for Colors

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import Colors from "../constants/colors.js";
+
 const Header = (props) => {
 	return (
 		<View style={styles.header}>
@@ -13,7 +15,7 @@ const styles = StyleSheet.create({
 	header: {
 		width: "100%",
 		height: 90,
-		backgroundColor: "#f7287b",
+		backgroundColor: Colors.primary,
 		paddingTop: 36,
 		alignItems: "center",
 		justifyContent: "center",
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
+import Card from "../components/Card.js";
+import Colors from "../constants/colors.js";
+
 const StartGameScreen = (props) => {
 	return (
 		<View style={styles.screen}>
 			<Text style={styles.title}>Start a New Game!</Text>
-			<View style={styles.inputContainer}>
+			<Card style={styles.inputContainer}>
 				<Text>Select a Number</Text>
 				<TextInput />
 				<View style={styles.buttonContainer}>
-					<Button title="Reset" onPress={() => {}} />
-					<Button title="Confirm" onPress={() => {}} />
+					<View style={styles.button}>
+						<Button title="Reset" color={Colors.accent} onPress={() => {}} />
+					</View>
+					<View style={styles.button}>
+						<Button title="Confirm" color={Colors.primary} onPress={() => {}} />
+					</View>
 				</View>
-			</View>
+			</Card>
 		</View>
 	);
 };
@@ -31,20 +38,15 @@ const styles = StyleSheet.create({
 		width: 300,
 		maxWidth: "80%",
 		alignItems: "center",
-		shadowColor: "black",
-		shadowOffset: { width: 0, height: 2 },
-		shadowRadius: 6,
-		shadowOpacity: 0.26,
-		elevation: 8,
-		backgroundColor: "white",
-		padding: 20,
-		borderRadius: 10,
 	},
 	buttonContainer: {
 		flexDirection: "row",
 		width: "100%",
 		justifyContent: "space-between",
 		paddingHorizontal: 15,
+	},
+	button: {
+		width: 100,
 	},
 });
 export default StartGameScreen;
