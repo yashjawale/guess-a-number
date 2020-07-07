36bf1e88fd8d57e728f961dd639b2527b257f022
Yash-Jawale <63059729+Yash-Jawale@users.noreply.github.com>
2020-07-07 20:31:00 +0530

    Added game screen

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
 import Header from "./components/Header.js";
 import StartGameScreen from "./screens/StartGameScreen.js";
+import GameScreen from "./screens/GameScreen.js";
 
 export default function App() {
+	const [userNumber, setUserNumber] = useState();
+
+	const startGameHandler = (selectedNumber) => {
+		setUserNumber(selectedNumber);
+	};
+
+	let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+	if (userNumber) {
+		content = <GameScreen userChoice={userNumber} />;
+	}
 	return (
 		<View style={styles.screen}>
 			<Header title="Guess a Number" />
-			<StartGameScreen />
+			{content}
 		</View>
 	);
 }
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,8 @@
-import react, { useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Button, StyleSheet } from "react-native";
+
+import NumberContainer from "../components/NumberContainer.js";
+import Card from "../components/Card.js";
 
 const generateRandomInBetween = (min, max, exclude) => {
 	min = Math.ceil(min);
@@ -16,8 +19,44 @@ const GameScreen = (props) => {
 	const [currentGuess, setCurrentGuess] = useState(
 		generateRandomInBetween(1, 100, props.userChoice)
 	);
+
+	const nextGuessHandler = (direction) => {};
+
+	return (
+		<View style={styles.screen}>
+			<Text>Opponent's Guess</Text>
+			<NumberContainer>{currentGuess}</NumberContainer>
+			<Card style={styles.buttonContainer}>
+				<Button
+					title="LOWER"
+					onPress={() => {
+						nextGuessHandler.bind(this, "lower");
+					}}
+				/>
+				<Button
+					title="GREATER"
+					onPress={() => {
+						nextGuessHandler.bind(this, "greater");
+					}}
+				/>
+			</Card>
+		</View>
+	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		padding: 10,
+		alignItems: "center",
+	},
+	buttonContainer: {
+		flexDirection: "row",
+		justifyContent: "space-around",
+		width: 300,
+		maxWidth: "80%",
+		marginTop: 20,
+	},
+});
 
 export default GameScreen;
