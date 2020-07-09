c47396e26c84fbfca79077c0b1795f861f134e05
Yash-Jawale <63059729+Yash-Jawale@users.noreply.github.com>
2020-07-09 21:09:08 +0530

    Added Stylse to the Image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -8,7 +8,13 @@ const GameOverScreen = (props) => {
 	return (
 		<View style={styles.screen}>
 			<TitleText>The Game is Over!</TitleText>
-			<Image source={require("../assets/success.png")} />
+			<View style={styles.imageContainer}>
+				<Image
+					style={styles.image}
+					source={require("../assets/success.png")}
+					resizeMode="cover"
+				/>
+			</View>
 			<BodyText>No. of rounds: {props.roundsNumber}</BodyText>
 			<BodyText>Number was: {props.userNumber}</BodyText>
 			<Button title="NEW GAME" onPress={props.onRestart} />
@@ -21,6 +27,19 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center",
+	},
+	imageContainer: {
+		width: 300,
+		height: 300,
+		borderRadius: 150,
+		borderWidth: 3,
+		borderColor: "black",
+		overflow: "hidden",
+		marginVertical: 30,
+	},
+	image: {
+		width: "100%",
+		height: "100%",
 	},
 });
 
